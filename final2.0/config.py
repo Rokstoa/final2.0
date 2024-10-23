4af18c5cabd3fbe5d93b049c2705ae093c0eb8d4
Rokstoa <rokstoa@gmail.com>
2024-10-23 21:49:43 +0200

    Delete final2.0 directory

--- final2.0/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'TGVYj17xDAi4CFqAaMtSSZsZc4RuB18q'
